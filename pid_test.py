2b9e24d59c51df6a9a2869456f2201df7b9bd9ac
VJMReichenbach <Valentin.Reichenbach@live.com>
2022-09-10 17:42:09 +0200

    stated working on the random data generation

--- pid_test.py
+++ pid_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from simple_pid import PID
+import matplotlib.pyplot as plt
+from perlin_noise import PerlinNoise
 import argparse
 
 ver = "0.0.1"
@@ -14,16 +16,35 @@ Version: {ver}
 License: GPLv3+
 """
 
-def debugMode():
-    print('TODOOOO')
+def generateNoise(octaves: float=5, seed: int=1):
+    """Generates the random dataset with perlin noise"""
+    noise = PerlinNoise(octaves=octaves, seed=seed)
+    xP = 10
+    yP = 10
+    for x in range(0,9):
+        for y in range(0,9):
+            print(noise.noise([x/xP, y/yP]))
+    return 0
 
-def normalMode():
-    print('TODOO: EPICS interface not yet implemented')
+def generateData(args):
+    pid = PID(Kp=args.proportional, Kd=args.derivative, Ki=args.integral, setpoint=args.niveau)
+
+def debugMode(args):
+    generateNoise()
+    generateData(args)
+
+def normalMode(args):
+    # TODOO: Epics interface
+    print('EPICS interface not yet implemented')
     exit()
 
 def main():
     parser = argparse.ArgumentParser(description=description, epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
 
+    parser.add_argument('-p', '--proportional',type=float, default=0.5, help='specifys the coefficient for the proportional term')
+    parser.add_argument('-i', '--integral',type=float, default=0.5, help='specifys the coefficient for the integral term')
+    parser.add_argument('-d', '--derivative',type=float, default=0, help='specifys the coefficient for the derivative term')
+    parser.add_argument('-n', '--niveau', type=float, default=1, help='specifys the niveau that the PID controler should aim for')
     parser.add_argument('--debug', action='store_true', default=False, help="uses a test enviroment instead of the epics interface")
     parser.add_argument('--version', action='version', version=ver)
 
@@ -31,9 +52,9 @@ def main():
     args = parser.parse_args()
 
     if args.debug == True:
-        debugMode()
+        debugMode(args)
     else:
-        normalMode()
+        normalMode(args)
 
     
 if __name__ == '__main__':
